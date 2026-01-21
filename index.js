81d3eba42cc0605ccb27dec7c6c9a3e5098d187b
Dean Howe <hideanhowe@gmail.com>
2026-01-21 17:53:09 +0000

    solution

--- index.js
+++ index.js
@@ -10,3 +10,31 @@
 // "hat"	19.95
 // "socks"	15.00
 // "jacket"	109.95
+
+function getsFreeShipping(cart, minimum) {
+  console.log(cart, minimum);
+
+  // find prices from items
+  const list = {
+    shirt: 34.25,
+    jeans: 48.5,
+    shoes: 75.0,
+    hat: 19.95,
+    socks: 15.0,
+    jacket: 109.95,
+  };
+
+  // setup counter
+  let counter = 0;
+
+  // loop through cart
+  for (let i = 0; i < cart.length; i++) {
+    if (list[cart[i]]) {
+      counter += list[cart[i]];
+    }
+  }
+
+  console.log(counter);
+  return counter > minimum;
+  //return cart;
+}
